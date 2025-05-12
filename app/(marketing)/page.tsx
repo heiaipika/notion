95d9a9737ba8c72937e6063f3c0cbe3ae463c674
gan <ganhx36@gmail.com>
2025-05-12 22:13:03 +0800

    delete: footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,5 @@
 import { Heading } from "./_components/heading";
 import { Heroes } from "./_components/heroes";
-import { Footer } from "./_components/footer";
 
 const MarketingPage = () => {
   return ( 
@@ -8,7 +7,6 @@ const MarketingPage = () => {
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-8 flex-1 px-6 pb-10">
          <Heading />
          <Heroes />
-         <Footer />
       </div>
     </div>
   );
